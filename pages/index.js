f438fe09d37e2dc02933866afb934f5d49948bd7
Saddat Hasan <saddathasan94@gmail.com>
2021-09-23 00:09:15 +0600

    git push testing

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ const Home = () => {
     <>
       <h1>Hello world</h1>
       <h2>upb8 rocks</h2>
+      <p>hi there is nothing to do </p>
+      <h3>what the hell is wrong with this terminal</h3><
     </>
   );
 };
