9874b54cd47a164b1f8c97fd203872848dee3e24
saddathasan <saddathasan94@gmail.com>
2021-10-03 14:49:21 +0600

    fixed the stupid bug?

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,12 @@ const Home = () => {
       <h1>Hello world</h1>
       <h2>upb8 rocks</h2>
       <p>hi there is nothing to do </p>
-      <h3>what the hell is wrong with this terminal</h3><
+      <h3>what the hell is wrong with this terminal</h3>
+
+      <Navigation />
     </>
-    <Navigation />
+    
+    
   );
 };
 
